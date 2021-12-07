833502fef856ed58fd3dd7de83c79fc9232da555
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-07 16:01:19 +0000

    async and await function added

--- index.js
+++ index.js
@@ -1,30 +1,72 @@
-const renderFilter =function(userInput){
-    fetch("https://jsonplaceholder.typicode.com/users")
-.then(response => response.json())
-.then(users => {
-    let table = document.querySelector("table")
+// const renderFilter = function(userInput){
+//     fetch("https://jsonplaceholder.typicode.com/users")
+// .then(response => response.json())
+// .then(users => {
+//     let ul = document.querySelector("ul")
     
-    users.forEach(data => {
-        table.innerHTML += `<tr>
-                                <td>${data.name}</td>
-                            </tr>`
+//    ul.innerHTML=""
+//     users.forEach(data => {
+     
+//      if(userInput === "address"){
+//         let address= JSON.stringify(data.address)
+//         ul.innerHTML += `<li>  ${address}</li>`
+//      }else
+//         ul.innerHTML += `<li>  ${data[userInput]}</li>`
        
-    })
-})
+       
+//     })
+// })
+// }
+
+const renderFilter = async (userInput) => {  
+
+    let response = await fetch('https://jsonplaceholder.typicode.com/users');  
+    let users = await response.json()    
+    let ul = document.querySelector("ul")
+    
+       ul.innerHTML=""
+        users.forEach(data => {
+         
+         if(userInput === "address"){
+            let address= JSON.stringify(data.address)
+            ul.innerHTML += `<li>  ${address}</li>`
+         }else
+            ul.innerHTML += `<li>  ${data[userInput]}</li>`
+        })
 }
+
 
 const filterName = function(event){
     let userInput = event.target.value;
-    renderFilter(userInput)
+    renderFilteredName(userInput)
 
+}
+
+renderFilteredName = function(userInput){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => {
+        let table = document.querySelector("table")
+        
+        const filteredData = (users.filter((user)=> user.name.toLowerCase().includes(userInput.toLowerCase())))
+        filteredData.forEach(data =>{
+            
+            table.innerHTML += `<tr>
+                                <td>  ${data[userInput]}</td>
+                            </tr>`
+        })
+    })
 }
 
 const displayNames = function(event){
-  
-    renderFilter()
+    let name= "name"
+    renderFilter(name)
 }
 
-const displayAddress
+const displayAddress =function(){
+    let address ="address"
+    renderFilter(address)
+}
 
 window.onload =function(){
 
