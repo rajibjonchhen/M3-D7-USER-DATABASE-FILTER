4549a547480c92b4116cdd02b9565b9bc85fa784
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-07 22:37:32 +0000

    extra information added

--- index.js
+++ index.js
@@ -17,15 +17,16 @@ const renderFilter = async (userInput) => {
 
     let response = await fetch('https://jsonplaceholder.typicode.com/users');  
     let users = await response.json()   
-    let ul = document.querySelector("ul")
-    ul.innerHTML=""
+    let ul1 = document.querySelector("ul:first-of-type")
+    let ul2 = document.querySelector("ul:last-of-type")
+    ul1.innerHTML=""
+    ul2.innerHTML=""
         users.forEach(data => {  
         if(userInput === "address"){
         
-            ul.innerHTML += `<li class="p-2 text-left">${data.address.street}, ${data.address.suite} ,${data.address.city}, ${data.address.zipcode}, ${data.address.geo.lat}, ${data.address.geo.lng}</li>
-            `
+            ul1.innerHTML += `<li class="p-2 text-left">${data.address.street}, ${data.address.suite} ,${data.address.city}, ${data.address.zipcode}, ${data.address.geo.lat}, ${data.address.geo.lng}</li>`
         }else
-            ul.innerHTML += `<li>  ${data[userInput]}</li>`
+            ul2.innerHTML += `<li class="p-2 text-left">  ${data[userInput]}</li>`
         })
 }
 
@@ -41,18 +42,16 @@ const loadAll = function(userInput){
 const filterUser = function(event){
     let userInput = event.target.value;
     let category = document.querySelector("select").value
-    console.log(userInput)
-    console.log(category)
-    
     fetch("https://jsonplaceholder.typicode.com/users")
     .then(response => response.json())
-    .then(users => {
-        
-        const filteredData = (users.filter((user)=> user['name'].toLowerCase().includes(userInput.toLowerCase())))
-    
+    .then(users => { 
+        const filteredData = (users.filter((user)=> user[category].toLowerCase().includes(userInput.toLowerCase())))
         display(filteredData) 
-        
         })
+}
+
+const sortData = function(){
+
 }
 
 // display all the information in the table
@@ -66,42 +65,53 @@ const display= function(users){
                         <td class="p-2 text-left">${data.username}</td>
                         <td class="p-2 text-left">${data.email}</td>
                         <td class="p-2 text-left">${data.address.street} ${data.address.suite} ${data.address.city} ${data.address.zipcode} ${data.address.geo.lat} ${data.address.geo.lng}</td>
-                        <td class="p-2 text-left"> <button class="btn btn-small bg-primary" userId="${data.id} onclick='filterName("name")'>know more</button>                       
-                        <div class="extraInfo">
+                        <td class="p-2 text-left">
                         
+                        
+                        
+
+                        <button type="button" onclick="extraInfo(event)" id="${data.id}">
+                          See more
+                        </button>
+                        <div class=" h-100 userInfo">
+
                         </div>
-                        </td>
-                        `
+                   
+                        </td> </tr>`
+                        
     })
 }
 
 
+const extraInfo = function(event){
+    let card = document.querySelector(".userInfo")
+    card.style.display ="block"
+    let userId = event.target.id
+    console.log(userId)
+    fetch("https://jsonplaceholder.typicode.com/users/" + userId)
+    .then(response=> response.json())
+    .then(jsonData => {
+        let container = document.querySelector(".userInfo")
+        container.innerHTML=""
+        let div = document.querySelector(".userInfo")
+        // div.innerHTML=""
+      container.innerHTML =`<div class="card" style="width: 18rem;">
+    
+            <h5 class="card-title">${jsonData.company.name}</h5>
+            <p class="card-text">${jsonData.company.catchPhrase}</p>
+            <p class="card-text">${jsonData.company.bs}</p>
+        </div>
+        </div>
+        `
+    
+    })
+}
 
 window.onload =function(){
 loadAll()
 
 
-const extraInfo = function(event){
-        let userId= event.target.userId
-        console.log(userId)
-        fetch("https://jsonplaceholder.typicode.com/users/" + 1)
-        .then(response=> response.json())
-        .then(jsonData => {
-            let div = document.querySelector("#extraInfo")
-            console.log(jsonData)
-            
-            div.innerHTML =`<div class="card" style="width: 18rem;">
-            
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">${jsonData.company.name}</p>
-                <p class="card-text">${jsonData.company.catchPhrase}</p>
-                <p class="card-text">${jsonData.company.bs}</p>
-                
-            </div>
-            </div>`
-        })
-    }
+
 
 
     fetch("https://jsonplaceholder.typicode.com/users/" + 1)
